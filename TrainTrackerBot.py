d22bbd85737a78f7b2975e76c2bbcb3d9b3df2c4
nu11-o <bluebananaaa594@gmail.com>
2026-02-17 13:21:01 +0000

    added load_dotenv()

--- TrainTrackerBot.py
+++ TrainTrackerBot.py
@@ -9,6 +9,8 @@ import os
 import aiohttp
 import asyncio
 
+load_dotenv()
+
 intents = discord.Intents.default()
 intents.message_content = True
 bot = commands.Bot(command_prefix="!", intents=intents, case_insensitive=True)
